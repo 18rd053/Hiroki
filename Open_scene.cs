5d79143e8d56f6da4ed09363e05134ee048cf213
18rd053 <65531548+18rd053@users.noreply.github.com>
2021-03-24 18:42:46 +0900

    Add files via upload

---
+++ Open_scene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_scene : MonoBehaviour
+{
+    [SerializeField]
+    private Change_scene_panel Mask;
+
+    
+    void Start()
+    {
+        Mask.Open_scene_action();
+    }
+
+}
