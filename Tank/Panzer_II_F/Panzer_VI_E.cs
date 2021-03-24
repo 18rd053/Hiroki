04dcee9ae350d53d30aeed54581a0188b5c4adb8
18rd053 <65531548+18rd053@users.noreply.github.com>
2021-03-24 19:01:59 +0900

    Add files via upload

---
+++ Tank/Panzer_II_F/Panzer_VI_E.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panzer_VI_E : MonoBehaviour
+{ 
+    /// <summary>
+    /// Panzer_VI_E本体に取り付けるスクリプト
+    /// タンクの情報を格納
+    /// SPattackを管理
+    /// </summary>
+
+    ///Tank_moveに渡す情報
+    public float Tank_speed = 500f;
+    public float Speed_limit = 50f;
+    Vector3 Tank_rote = new Vector3(0, -2f, 0);
+    Rigidbody Tank_rb;
+
+
+    /// Tank_shotに渡す情報
+    [SerializeField]
+    private GameObject Gun_pos;
+    public float Bullet_reload_time = 4f;
+    public int Bullet_attack_point = 20;
+
+
+    /// Tank_HPに渡す情報
+    public int Hp = 200;
+
+    /// Tank_SPattackに渡す固有のSPattack時BGM
+    [SerializeField]
+    private AudioClip SPattack_BGM;
+
+
+    public void Awake()
+    {
+
+        Tank_rb = GetComponent<Rigidbody>();
+
+        //Tank_moveに機体性能をセット
+        GetComponent<Tank_move>()
+            .Set_move(Tank_rb, Tank_speed, Speed_limit, Tank_rote);
+
+        GetComponent<Tank_move>()
+            .Set_flg(true);
+
+        //Tank_shotに射撃性能をセット
+        Gun_pos.GetComponent<Tank_shot>()
+            .Set_shot(Bullet_reload_time, Bullet_attack_point);
+
+        //Tank_HPに最大HPをセット
+        GetComponent<Tank_HP>()
+            .Set_hp(Hp);
+
+        //Tank_SPattackに専用BGMをセット
+        GetComponent<Tank_SPattack>()
+            .Set_SPattack_BGM(SPattack_BGM);
+    }
+
+}
+
---
+++ Tank/Panzer_II_F/Panzer_VI_E_SP.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panzer_VI_E_SP : MonoBehaviour
+{
+    //SPattackを発動していいか監視
+    [SerializeField]
+    private Tank_SPattack _Tank_SPattack;
+
+    /// <summary>
+    /// Panzer_VI_EのSPattackは5発分威力UPリロードDown
+    /// </summary>
+
+
+    /// Tank_shotに渡す情報
+    [SerializeField]
+    private Tank_shot _Tank_shot;
+    public float Bullet_reload_time = 4f;
+    public float Sp_bullet_reload_time = 5f;
+    public int Bullet_attack_point = 20;
+    public int Sp_bullet_attack_point = 30;
+
+    //効果時間
+    private int Capacity = 5;
+
+    //初期弾数の0と必殺技終了の0を見分けるためのフラグ
+    private bool SP_flg = false;
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        if (_Tank_SPattack.Check_SPattack())
+        {
+            SP_flg = true;
+            SPattack_active();
+        }
+        //残り強化弾数が0以下なら必殺技を終了
+        if(_Tank_shot.Bullet_Capacity <= 0 && SP_flg)
+        {
+            SPattack_end();
+        }
+    }
+
+    public void SPattack_active()
+    {
+        //５発分の強化弾をセット
+        _Tank_shot.Set_Capacity(Capacity);
+        _Tank_shot.Set_shot(Sp_bullet_reload_time, Sp_bullet_attack_point);
+    }
+
+    //SPattack終了
+    public void SPattack_end()
+    {
+        _Tank_shot.Set_shot(Bullet_reload_time, Bullet_attack_point);
+
+        SP_flg = false;
+
+        //演出をoffに
+        _Tank_SPattack.SPattack_end();
+    }
+}
