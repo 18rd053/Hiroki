1eea9cec9288e770901efad0c9779b19d002ea37
18rd053 <65531548+18rd053@users.noreply.github.com>
2021-03-24 19:00:22 +0900

    Add files via upload

---
+++ Tank/Cromwell_IV/Cromwell_IV.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cromwell_IV : MonoBehaviour
+{
+    /// <summary>
+    /// クロムウェル本体に取り付けるスクリプト
+    /// タンクの情報を格納
+    /// SPattackを管理
+    /// </summary>
+
+    ///Tank_moveに渡す情報
+    public float Tank_speed = 700f;
+    public float Speed_limit = 60f;
+    Vector3 Tank_rote = new Vector3(0, -2f, 0);
+    Rigidbody Tank_rb;
+
+
+    /// Tank_shotに渡す情報
+    [SerializeField]
+    private GameObject Gun_pos;
+    public float Bullet_reload_time = 3f;
+    public int Bullet_attack_point = 10;
+
+
+    /// Tank_HPに渡す情報
+    public int Hp = 100;
+
+    /// Tank_SPattackに渡す固有のSPattack時BGM
+    [SerializeField]
+    private AudioClip SPattack_BGM;
+
+
+    public void Awake()
+    {
+
+        Tank_rb = GetComponent<Rigidbody>();
+
+        //Tank_moveに機体性能をセット
+        GetComponent<Tank_move>()
+            .Set_move(Tank_rb, Tank_speed, Speed_limit, Tank_rote);
+
+        GetComponent<Tank_move>()
+            .Set_flg(true);
+
+        //Tank_shotに射撃性能をセット
+        Gun_pos.GetComponent<Tank_shot>()
+            .Set_shot(Bullet_reload_time, Bullet_attack_point);
+
+        //Tank_HPに最大HPをセット
+        GetComponent<Tank_HP>()
+            .Set_hp(Hp);
+
+        //Tank_SPattackに専用BGMをセット
+        GetComponent<Tank_SPattack>()
+            .Set_SPattack_BGM(SPattack_BGM);
+    }
+
+}
---
+++ Tank/Cromwell_IV/Cromwell_IV_SP.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cromwell_IV_SP : MonoBehaviour
+{
+    //SPattackを発動していいか監視
+    [SerializeField]
+    private Tank_SPattack _Tank_SPattack;
+
+    /// <summary>
+    /// CromwellのSPattackは30秒間リロード時間短縮
+    /// </summary>
+
+
+    /// Tank_shotに渡す情報
+    [SerializeField]
+    private Tank_shot _Tank_shot;
+    public float Bullet_reload_time = 3f;
+    public float Sp_bullet_reload_time = 1f;
+    public int Bullet_attack_point = 10;
+
+    //効果時間
+    private float SP_time = 30f;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (_Tank_SPattack.Check_SPattack())
+        {
+            SPattack_active();
+        }
+    }
+
+    public void SPattack_active()
+    {
+        _Tank_shot.Set_shot(Sp_bullet_reload_time, Bullet_attack_point);
+        Invoke("SPattack_end", SP_time);
+    }
+
+    //SPattack終了
+    public void SPattack_end()
+    {
+        _Tank_shot.Set_shot(Bullet_reload_time, Bullet_attack_point);
+
+        //演出をoffに
+        _Tank_SPattack.SPattack_end();
+    }
+}
