48877d18362c7627795c705b7019d4f197961540
18rd053 <65531548+18rd053@users.noreply.github.com>
2021-03-24 19:11:37 +0900

    Add files via upload

---
+++ Audio/Audio_controller.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using UnityEngine.UI;
+
+namespace Audio_keeper
+{
+    public class Audio_controller : MonoBehaviour
+    {
+        //BGM,SE,Voiceそれぞれの音量調整用
+
+        /// <summary>
+        /// シーン開始時
+        /// Audio_managerData - Slider - AudioSource
+        ///
+        /// 音量変更時
+        /// Slider - Audio_managerData
+        /// |_ AudioSource
+        /// </summary>
+
+        [SerializeField]
+        private Slider BGM_slider;
+
+        [SerializeField]
+        private Slider SE_slider;
+
+        [SerializeField]
+        private Slider Voice_slider;
+
+
+        //初期値は0.2
+        [SerializeField]
+        private AudioSource BGM;
+        //初期値は0.4
+        [SerializeField]
+        private AudioSource SE;
+        //初期値は0.5
+        [SerializeField]
+        private AudioSource Voice;
+
+
+        [SerializeField]
+        private AudioClip SE_check;
+
+        [SerializeField]
+        private AudioClip Voice_check;
+
+        [SerializeField]
+        private Audio_managerData _Audio_managerData;
+
+
+        //初めの変更を行うため
+        private bool Change_flg = true;
+
+        private void Awake()
+        {
+            _Audio_managerData = FindObjectOfType<Audio_manager>().Get_AudioManagerData();
+
+            
+        }
+
+        //SEとVoiceは戦車本体が持っているためFWTで取得
+        void Start()
+        {
+            SE = GameObject.FindWithTag("SE").GetComponent<AudioSource>();
+            Voice = GameObject.FindWithTag("Voice").GetComponent<AudioSource>();
+
+            Change_flg = false;
+            //オーディオマネージャーから前回のシーンと同じ音量を取得しスライダーに反映
+            BGM_slider.value = _Audio_managerData.Get_BGMVolume();
+            SE_slider.value = _Audio_managerData.Get_SEVolume();
+            Voice_slider.value = _Audio_managerData.Get_CVVolume();
+
+            Change_flg = true;
+
+            //スライダーの値からオーディオソースのボリュームを設定
+            Control_audio();
+        }
+
+        //各パラメータが操作された時に呼び出される
+        private void Control_audio()
+        {
+            //_Audio_managerData.Set_volume(BGM.volume, SE.volume, Voice.volume);
+            _Audio_managerData.Set_volume(BGM_slider.value, SE_slider.value, Voice_slider.value);
+
+            BGM.volume = BGM_slider.value;
+            SE.volume = SE_slider.value;
+            Voice.volume = Voice_slider.value;// * 7;
+            
+        }
+
+        //各パラメータが操作されたらスライダーから呼び出される
+        public void Check_BGM()
+        {
+            if (Change_flg)
+            {
+                Control_audio();
+            }
+        }
+
+        public void Check_SE()
+        {
+            if (Change_flg)
+            {
+                Control_audio();
+                SE.PlayOneShot(SE_check);
+            }
+        }
+
+        public void Check_Voice()
+        {
+            if (Change_flg)
+            {
+                Control_audio();
+                Voice.PlayOneShot(Voice_check);
+            }
+        }
+    }
+}
---
+++ Audio/BGM_controller.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_controller : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource BGM_source;
+
+    [SerializeField]
+    private AudioClip Def_BGM;
+
+    [SerializeField]
+    private AudioClip Win_BGM;
+
+    [SerializeField]
+    private AudioClip Lose_BGM;
+
+
+    private AudioClip SPattack_BGM;
+
+    private float _BGM_volume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM_source.clip = Def_BGM;
+        BGM_source.Play();
+        _BGM_volume = BGM_source.volume;
+    }
+
+ 
+    public void Set_SPattack_BGM(AudioClip _SPattack_BGM)
+    {
+        SPattack_BGM = _SPattack_BGM;
+    }
+
+    public void Change_BGM_SP(bool flg)
+    {
+        _BGM_volume = BGM_source.volume;
+        if (flg)
+        {
+            BGM_source.Stop();
+            BGM_source.clip = SPattack_BGM;
+            BGM_source.Play();
+            BGM_source.volume = _BGM_volume;
+        }
+        else if (!flg)
+        {
+            BGM_source.Stop();
+            BGM_source.clip = Def_BGM;
+            BGM_source.Play();
+            BGM_source.volume = _BGM_volume;
+        }
+    }
+
+    public void Change_BGM_Win(bool flg)
+    {
+        _BGM_volume = BGM_source.volume;
+        if (flg)
+        {
+            BGM_source.Stop();
+            BGM_source.clip = Win_BGM;
+            BGM_source.Play();
+            BGM_source.volume = _BGM_volume;
+        }
+        else if (!flg)
+        {
+            BGM_source.Stop();
+            BGM_source.clip = Def_BGM;
+            BGM_source.Play();
+            BGM_source.volume = _BGM_volume;
+        }
+    }
+
+    public void Change_BGM_Lose(bool flg)
+    {
+        _BGM_volume = BGM_source.volume;
+        if (flg)
+        {
+            BGM_source.Stop();
+            BGM_source.clip = Lose_BGM;
+            BGM_source.Play();
+            BGM_source.volume = _BGM_volume;
+        }
+        else if (!flg)
+        {
+            BGM_source.Stop();
+            BGM_source.clip = Def_BGM;
+            BGM_source.Play();
+            BGM_source.volume = _BGM_volume;
+        }
+    }
+}
